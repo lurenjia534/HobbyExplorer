db499edececb48e3c574965db185b5a8225c025e
luren <lurenjia534@outlook.com>
2024-06-18 16:25:41 +0800

    App: Improve the UI of the card
    * This completes the card's UI
    
    * We need to optimize the layout

--- app/src/main/java/com/lurenjia534/hobbyexplorer/MainActivity.kt
+++ app/src/main/java/com/lurenjia534/hobbyexplorer/MainActivity.kt
@@ -87,15 +87,20 @@ fun HomeScreen(navController: NavHostController) {
     }
 
     Scaffold(
-        floatingActionButton = { RefreshFAB(hobbyViewModel) }
+        floatingActionButton = { RefreshFAB(hobbyViewModel) },
     ) { innerPadding ->
-        Column(modifier = Modifier
-            .fillMaxSize()
-            .padding(innerPadding)) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
             if (isLoading) {
                 Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                     Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        LinearProgressIndicator(color = MaterialTheme.colorScheme.primary, strokeCap = StrokeCap.Round)
+                        LinearProgressIndicator(
+                            color = MaterialTheme.colorScheme.primary,
+                            strokeCap = StrokeCap.Round
+                        )
                         Spacer(modifier = Modifier.height(8.dp))
                         Text(
                             text = "Loading..",
@@ -133,10 +138,12 @@ fun RefreshFAB(hobbyViewModel: HobbyViewModel) {
 
 @Composable
 fun HobbyCard(hobby: Hobby, onClick: () -> Unit) {
-    Card(modifier = Modifier
-        .fillMaxWidth()
-        .padding(8.dp)
-        .clickable(onClick = onClick)) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable(onClick = onClick)
+    ) {
         Column(modifier = Modifier.padding(16.dp)) {
             Text(text = hobby.info ?: "No Info")
             Text(text = hobby.nicheInfo ?: "没内容")
@@ -157,6 +164,26 @@ fun All(navController: NavHostController) {
     Text(text = "Profile Screen")
 }
 
+@Composable
+fun IpInfoText(label: String, value: String) {
+    Column {
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.primary,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Default
+        )
+        Text(
+            text = value,
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onSurface,
+            fontWeight = FontWeight.Normal,
+            fontFamily = FontFamily.Default
+        )
+    }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailsScreen(hobbyId: String) {
@@ -169,25 +196,124 @@ fun DetailsScreen(hobbyId: String) {
     Scaffold(
     ) { innerPadding ->
         hobby?.let {
-            Column(
+            LazyColumn(
                 modifier = Modifier
                     .padding(innerPadding)
                     .fillMaxSize()
             ) {
-                Text(text = it.info ?: "No Info", style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold))
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(text = it.nicheInfo ?: "没内容", style = TextStyle(fontSize = 18.sp))
-                Text(text = it.contactTime ?: "没内容", style = TextStyle(fontSize = 18.sp))
-                Text(text = it.costCount ?: "没内容", style = TextStyle(fontSize = 18.sp))
-                Text(text = it.timeOfDay ?: "没内容", style = TextStyle(fontSize = 18.sp))
-                Text(text = it.level ?: "没内容", style = TextStyle(fontSize = 18.sp))
-                Text(text = it.levelVal.toString(), style = TextStyle(fontSize = 18.sp))
-                Text(text = it.putIntoCostLevel ?: "没内容", style = TextStyle(fontSize = 18.sp))
-                Text(text = it.putIntoTimeLevel ?: "没内容", style = TextStyle(fontSize = 18.sp))
-            }
-        } ?: run {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                Text(text = "Loading...")
+                item {
+                    Text(
+                        text = it.info ?: "No Info",
+                        style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Card(
+                        modifier = Modifier.fillMaxWidth(),
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(32.dp)
+                        ) {
+                            IpInfoText(label = "基本介绍", value = it.nicheInfo ?: "没内容")
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+
+                    ) {
+                        Card(
+                        ) {
+                            Column(
+                                modifier = Modifier.padding(32.dp)
+                            ) {
+                                IpInfoText(label = "时间成本", value = it.contactTime ?: "没内容")
+                            }
+                        }
+                        Card(
+                        ) {
+                            Column(
+                                modifier = Modifier.padding(32.dp)
+                            ) {
+                                IpInfoText(label = "兴趣花销", value = it.costCount ?: "没内容")
+                            }
+                        }
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+
+                    ) {
+                        Card(
+                        ) {
+                            Column(
+                                modifier = Modifier.padding(32.dp)
+                            ) {
+                                IpInfoText(label = "一天需要时间", value = it.timeOfDay ?: "没内容")
+                            }
+                        }
+                        Card(
+                        ) {
+                            Column(
+                                modifier = Modifier.padding(32.dp)
+                            ) {
+                                IpInfoText(
+                                    label = "建议者自我认知水准",
+                                    value = it.level ?: "没内容"
+                                )
+                            }
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Card() {
+                        Column(modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(32.dp)) {
+                            Text(
+                                text = "建议者自我认知水准可视化",
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = MaterialTheme.colorScheme.primary,
+                                fontWeight = FontWeight.Bold,
+                                fontFamily = FontFamily.Default
+                            )
+                            Spacer(modifier = Modifier.height(4.dp))
+                            LinearProgressIndicator(
+                                progress = { (it.levelVal?.div(4f))?.toFloat() ?: 0f },
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+                    }
+                    Card(
+                        modifier = Modifier.fillMaxWidth(),
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(32.dp)
+                        ) {
+                            IpInfoText(
+                                label = "投入成本水平",
+                                value = it.putIntoCostLevel ?: "没内容"
+                            )
+                        }
+                    }
+                    Card(
+                        modifier = Modifier.fillMaxWidth(),
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(32.dp)
+                        ) {
+                            IpInfoText(
+                                label = "投入时间级别",
+                                value = it.putIntoTimeLevel ?: "没内容"
+                            )
+                        }
+                    }
+                    //     Text(text = it.putIntoCostLevel ?: "没内容", style = TextStyle(fontSize = 18.sp))
+                    //     Text(text = it.putIntoTimeLevel ?: "没内容", style = TextStyle(fontSize = 18.sp))
+                }
+            } ?: run {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Loading...")
+                }
             }
         }
     }
